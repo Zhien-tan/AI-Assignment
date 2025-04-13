e40fa568a4cc403600615cb491f4f5fd4794eb4e
Zhien-tan <tanze-am22@student.tarc.edu.my>
2025-04-13 18:35:08 +0800

    Romove app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-import streamlit as st
-from predict import predict_sentiment
 
-st.title("☕ Coffee Review Sentiment Classifier")
-st.write("Enter a coffee review below to predict whether it's Positive, Neutral, or Negative.")
-
-user_input = st.text_area("Enter your review here:")
-
-if st.button("Predict"):
-    if user_input.strip() == "":
-        st.warning("Please enter a review!")
-    else:
-        sentiment, confidence = predict_sentiment(user_input)
-        st.success(f"Predicted Sentiment: **{sentiment}**")
-        st.info(f"Confidence Score: {confidence:.2f}")
