4c047a9dfa4dcbcad2270581067edc4c4e1ebd23
Zhien-tan <tanze-am22@student.tarc.edu.my>
2025-04-14 01:55:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-import streamlit as st
 import gdown
-import zipfile
-import os
-from transformers import BertForSequenceClassification, BertTokenizer
+import joblib
 
-# Download the zipped model from Google Drive
-model_url = 'https://drive.google.com/uc?id=1pKFpU56YyLloC5IONDMxih5QMQSew54B'  # Replace with your file ID
+# Download the model from Google Drive
+model_url = 'https://drive.google.com/uc?id=1pKFpU56YyLloC5IONDMxih5QMQSew54B'
 gdown.download(model_url, 'sentiment_model.pkl', quiet=False)
 
-# Unzip the model
-if not os.path.exists('sentiment_model'):
-    os.makedirs('sentiment_model')
-    
-with zipfile.ZipFile('sentiment_model.pkl', 'r') as zip_ref:
-    zip_ref.extractall('sentiment_model')
-
-# Load the model and tokenizer
-model = BertForSequenceClassification.from_pretrained('sentiment_model')
-tokenizer = BertTokenizer.from_pretrained('sentiment_model')
+# Load the model using joblib
+model = joblib.load('sentiment_model.pkl')
 
 # Function to predict sentiment
 def predict_sentiment(text):
